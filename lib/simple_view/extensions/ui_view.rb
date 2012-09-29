4a637558bfcacaed2c80e34e15a7c61277edb7f0
Sean Ho <sean.ho@thoughtworks.com>
2012-09-29 14:06:58 +1000

    Safer UIView monkey patching

--- lib/simple_view/extensions/ui_view.rb
+++ lib/simple_view/extensions/ui_view.rb
@@ -1,80 +1,84 @@
-class UIView
-  attr_accessor :name, :bottom, :right, :anchors
+module SimpleView
+  module UIView
+    attr_accessor :name, :bottom, :right, :anchors
 
-  def find name
-    subviews.each do |subview|
-      return subview if subview.name == name
+    def find name
+      subviews.each do |subview|
+        return subview if subview.name == name
+      end
     end
-  end
-  alias_method :subview, :find
+    alias_method :subview, :find
 
-  def invalidate_size
-    f = self.frame
-    max_width = superview ? superview.bounds.size.width : 0
-    max_height = superview ? superview.bounds.size.height : 0
+    def invalidate_size
+      f = self.frame
+      max_width = superview ? superview.bounds.size.width : 0
+      max_height = superview ? superview.bounds.size.height : 0
 
-    @anchors ||= [:top, :left]
-    anchor_top = @anchors.include?(:top)
-    anchor_left = @anchors.include?(:left)
-    anchor_bottom = @anchors.include?(:bottom)
-    anchor_right = @anchors.include?(:right)
+      @anchors ||= [:top, :left]
+      anchor_top = @anchors.include?(:top)
+      anchor_left = @anchors.include?(:left)
+      anchor_bottom = @anchors.include?(:bottom)
+      anchor_right = @anchors.include?(:right)
 
-    self.autoresizingMask = UIViewAutoresizingNone
-    self.autoresizingMask |= UIViewAutoresizingFlexibleTopMargin unless anchor_top
-    self.autoresizingMask |= UIViewAutoresizingFlexibleLeftMargin unless anchor_left
-    self.autoresizingMask |= UIViewAutoresizingFlexibleBottomMargin unless anchor_bottom
-    self.autoresizingMask |= UIViewAutoresizingFlexibleRightMargin unless anchor_right
-    self.autoresizingMask |= UIViewAutoresizingFlexibleWidth if anchor_left && anchor_right
-    self.autoresizingMask |= UIViewAutoresizingFlexibleHeight if anchor_top && anchor_bottom
+      self.autoresizingMask = UIViewAutoresizingNone
+      self.autoresizingMask |= UIViewAutoresizingFlexibleTopMargin unless anchor_top
+      self.autoresizingMask |= UIViewAutoresizingFlexibleLeftMargin unless anchor_left
+      self.autoresizingMask |= UIViewAutoresizingFlexibleBottomMargin unless anchor_bottom
+      self.autoresizingMask |= UIViewAutoresizingFlexibleRightMargin unless anchor_right
+      self.autoresizingMask |= UIViewAutoresizingFlexibleWidth if anchor_left && anchor_right
+      self.autoresizingMask |= UIViewAutoresizingFlexibleHeight if anchor_top && anchor_bottom
 
-    if (anchor_left && anchor_right) || (anchor_left && !@right.nil?)
-      f.size.width = max_width - self.left - @right.to_i
-    elsif anchor_right
-      f.origin.x = max_width - self.width - @right.to_i
-    elsif !anchor_left && !anchor_right
-      f.origin.x = max_width / 2 - f.size.width / 2
+      if (anchor_left && anchor_right) || (anchor_left && !@right.nil?)
+        f.size.width = max_width - self.left - @right.to_i
+      elsif anchor_right
+        f.origin.x = max_width - self.width - @right.to_i
+      elsif !anchor_left && !anchor_right
+        f.origin.x = max_width / 2 - f.size.width / 2
+      end
+
+      if (anchor_top && anchor_bottom) || (anchor_top && !@bottom.nil?)
+        f.size.height = max_height - self.top - @bottom.to_i
+      elsif anchor_bottom
+        f.origin.y = max_height - self.height - @bottom.to_i
+      elsif !anchor_top && !anchor_bottom
+        f.origin.y = max_height / 2 - f.size.height / 2
+      end
+
+      self.frame = f
     end
 
-    if (anchor_top && anchor_bottom) || (anchor_top && !@bottom.nil?)
-      f.size.height = max_height - self.top - @bottom.to_i
-    elsif anchor_bottom
-      f.origin.y = max_height - self.height - @bottom.to_i
-    elsif !anchor_top && !anchor_bottom
-      f.origin.y = max_height / 2 - f.size.height / 2
+    def top
+      self.frame.origin.y
     end
 
-    self.frame = f
-  end
+    def setTop value
+      self.frame = [[self.frame.origin.x, value], [self.frame.size.width, self.frame.size.height]]
+    end
 
-  def top
-    self.frame.origin.y
-  end
+    def left
+      self.frame.origin.x
+    end
 
-  def setTop value
-    self.frame = [[self.frame.origin.x, value], [self.frame.size.width, self.frame.size.height]]
-  end
+    def setLeft value
+      self.frame = [[value, self.frame.origin.y], [self.frame.size.width, self.frame.size.height]]
+    end
 
-  def left
-    self.frame.origin.x
-  end
+    def width
+      self.frame.size.width
+    end
 
-  def setLeft value
-    self.frame = [[value, self.frame.origin.y], [self.frame.size.width, self.frame.size.height]]
-  end
+    def setWidth value
+      self.frame = [[self.frame.origin.x, self.frame.origin.y], [value, self.frame.size.height]]
+    end
 
-  def width
-    self.frame.size.width
-  end
+    def height
+      self.frame.size.height
+    end
 
-  def setWidth value
-    self.frame = [[self.frame.origin.x, self.frame.origin.y], [value, self.frame.size.height]]
-  end
-
-  def height
-    self.frame.size.height
-  end
-
-  def setHeight value
-    self.frame = [[self.frame.origin.x, self.frame.origin.y], [self.frame.size.width, value]]
+    def setHeight value
+      self.frame = [[self.frame.origin.x, self.frame.origin.y], [self.frame.size.width, value]]
+    end
   end
 end
+
+UIView.send(:include, SimpleView::UIView)
--- spec/extensions/ui_view_spec.rb
+++ spec/extensions/ui_view_spec.rb
@@ -1,20 +1,19 @@
-describe "UIViewExtensions" do
-  before do
+describe "SimpleView::UIView" do
+  describe "name accessor" do
+    it "should get and set view name" do
+      view = UIView.alloc.initWithFrame(CGRectZero)
+      view.name = "view_name"
+      view.name.should == "view_name"
+    end
   end
 
-  it "should get and set view name" do
-    view = UIView.alloc.initWithFrame(CGRectZero)
-    view.name = "view_name"
-    view.name.should == "view_name"
-  end
-  
   describe "dimensions" do
     it "should set left" do
       view = UIView.alloc.initWithFrame(CGRectZero)
       view.left = 10
       view.left.should == 10
     end
-    
+
     it "should set right" do
       superview = UIView.alloc.initWithFrame(CGRectMake(0, 0, 100, 100))
       view = UIView.alloc.initWithFrame(CGRectZero)
@@ -22,13 +21,13 @@ describe "UIViewExtensions" do
       view.right = 10
       view.right.should == 10
     end
-    
+
     it "should set top" do
       view = UIView.alloc.initWithFrame(CGRectZero)
       view.top = 11
       view.top.should == 11
     end
-    
+
     it "should set bottom" do
       superview = UIView.alloc.initWithFrame(CGRectMake(0, 0, 100, 100))
       view = UIView.alloc.initWithFrame(CGRectZero)
@@ -36,36 +35,36 @@ describe "UIViewExtensions" do
       view.bottom = 10
       view.bottom.should == 10
     end
-    
+
     it "should set width" do
       view = UIView.alloc.initWithFrame(CGRectZero)
       view.width = 12
       view.width.should == 12
     end
-    
+
     it "should set height" do
       view = UIView.alloc.initWithFrame(CGRectZero)
       view.height = 13
       view.height.should == 13
     end
   end
-  
-  describe "UIViewExtensions#find" do
+
+  describe "#find" do
     before do
       @parent_view = UIView.alloc.initWithFrame(CGRectZero)
       @child_view = UIView.alloc.initWithFrame(CGRectZero)
       @child_view.name = "nemo"
       @parent_view.addSubview(@child_view)
     end
-    
+
     it "should find subview by name" do
       @parent_view.find("nemo").should == @child_view
     end
-    
+
     it "should find subview by name using alias method" do
       @parent_view.subview("nemo").should == @child_view
     end
-    
+
     it "should return nil if subview cannot be found" do
       @parent_view.find("nemo").should == @child_view
     end
