9ab015b08d6332ab9a36ce7b88a85c16075730cd
Sean Ho <sean.ho@thoughtworks.com>
2013-01-21 14:16:12 +1100

    Fix UITextFieldBuilder inherits from UIViewBuilder instead of UIControlBuilder

--- lib/simple_view/builders/ui_text_field_builder.rb
+++ lib/simple_view/builders/ui_text_field_builder.rb
@@ -1,6 +1,6 @@
 module SimpleView
   module Builders
-    class UITextFieldBuilder < UIViewBuilder
+    class UITextFieldBuilder < UIControlBuilder
       include SimpleView::Builders::HasFont
       include SimpleView::Builders::HasTextColor
     end
